3aa0ea9c7bca2180b875997f326b95f5919dd365
kathycong <kathlynycong@gmail.com>
2023-03-09 00:00:58 +1300

    adding section 4.47 enemy hp bar

--- Section 4/battle/main.py
+++ Section 4/battle/main.py
@@ -1,6 +1,7 @@
 from classes.game import Person, bcolors
 from classes.magic import spell
 from classes.inventory import Item
+import random
 
 #Create black magic
 fire = spell("Fire", 25, 600, "black")
@@ -70,6 +71,8 @@ while running:
     for player in players:
         player.get_stats()
         print("\n")
+
+    enemy.get_enemy_stats()
 
     for player in players:
 
@@ -141,8 +144,14 @@ while running:
                 print(bcolors.OKGREEN + "\n" + item.name + "heals for", str(item.prop), "HP" + bcolors.ENDC)
                 
             elif  item.type == "elixer":
-                player.hp = player.maxhp
-                player.mp = player.maxmp
+
+                if item.name == "MegaElixer":
+                    for i in players:
+                        i.hp = i.maxhp
+                        i.mp = i.maxmp
+                else:
+                    player.hp = player.maxhp
+                    player.mp = player.maxmp
                 print(bcolors.OKGREEN + "\n" + item.name + "fully restores HP/MP" + bcolors.ENDC)
 
             elif item.type == "attack":
@@ -153,8 +162,10 @@ while running:
     #now we want the enemy to attack us
     enemy_choice = 1
 
+    target = random.randrange(0, 3)
     enemy_dmg = enemy.generate_damage()
-    player1.take_damage(enemy_dmg)
+
+    players[target].take_damage(enemy_dmg)
     print("Enemy attacks for", enemy_dmg) #, "Player HP", player.get_hp())
 
     print("===================================================")
