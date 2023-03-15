43cdfbf575c716bfaeffb85b73edaeca1df1cb8f
kathycong <kathlynycong@gmail.com>
2023-03-15 23:29:50 +1300

    adding section 5.59 Posting JSON

--- Section 6/requeststest.py
+++ Section 6/requeststest.py
@@ -1,27 +1,10 @@
-#Introduction to Request
-#pip3 install requests
-#pip3 install pillow
-
 import requests
-from  io import BytesIO
-from PIL import Image
 
-r = requests.get("https://wallpapercave.com/wp/TuVhQdr.jpg")
+my_data = {"name": "Kath", "email": "kath@example.com"}
+r = requests.post("https://www.w3schools.com/php/welcome.php", data = my_data)
 
-print("Status:", r.status_code) #returns status code 200 which refers to OK
+f = open("Section 6/myfile.html", "w+")
+f.write(r.text)
 
-#creating an image from the binary data that we get
-image = Image.open(BytesIO(r.content))
-
-print(image.size, image.format, image.mode)
-path = "./Section 6/image" + image.format
-
-
-#saving the image into local directory
-
-try:
-    image.save(path, image.format)
-except IOError:
-    print("Cannot save image.")
 
 
