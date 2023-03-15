fa29c111cccc84b316a2ecf089373e75d043ed7f
kathycong <kathlynycong@gmail.com>
2023-03-15 23:41:25 +1300

    Section 6.60 Headers

--- Section 6/requeststest.py
+++ Section 6/requeststest.py
@@ -4,9 +4,13 @@ import simplejson as json
 #creating a payload of data which refers to data being sent to the URL
 url = "https://www.googleapis.com/urlshortener/v1/url"
 payload = {"longurl": "http://example.com"}
+
+
 headers = {"ContentType": "application/json" }
 
 r = requests.post(url, json=payload)
+
+print(r.headers)
 
 print(json.loads(r.text)["error"]["code"])
 
