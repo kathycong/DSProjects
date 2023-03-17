0c4f7555a34ce2102b850945eadd85eca1cca704
kathycong <kathlynycong@gmail.com>
2023-03-18 01:14:46 +1300

    7.65 Improvements to image web scraper

--- Section 7/webscrapery/images.py
+++ Section 7/webscrapery/images.py
@@ -2,18 +2,30 @@ from bs4 import BeautifulSoup
 import requests
 from PIL import Image #used for importing images
 from io import BytesIO
+import os
 
-search = input("search for:")
-params = {"q": search}
-r = requests.get("http://www.bing.com/images/search", params = params)
+def StartSearch():
+    search = input("search for:")
+    params = {"q": search}
+    dir_name = search.replace(" ", "_").lower()
 
-soup = BeautifulSoup(r.text, "html.parser")
-links = soup.findAll("a", {"class": "thumb"})
+    if not os.path.isdir(dir_name):
+        os.makedirs(dir_name)
+    r = requests.get("http://www.bing.com/images/search", params = params)
 
-for item in links:
-    img_obj = requests.get(item.attrs["href"])
-    print("Getting", item.attrs["href"])
-    #getting the title of the image
-    title = item.attrs["href"].split("/")[-1] #getting the last element of the list using -1
-    img = Image.open(BytesIO(img_obj.content))
-    img.save("./scraped_images/" + title, img.format)
+    soup = BeautifulSoup(r.text, "html.parser")
+    links = soup.findAll("a", {"class": "thumb"})
+
+    for item in links:
+        img_obj = requests.get(item.attrs["href"])
+        print("Getting", item.attrs["href"])
+        #getting the title of the image
+        title = item.attrs["href"].split("/")[-1] #getting the last element of the list using -1
+        try:
+            img = Image.open(BytesIO(img_obj.content))
+            img.save("./" + dir_name + "/" + title, img.format)
+        except:
+            print("Could not save image")
+    
+    StartSearch()
+StartSearch()
