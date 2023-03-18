4e4e1052b2ded6bc9947bdf6ff3e5a8209c6910d
kathycong <kathlynycong@gmail.com>
2023-03-19 00:28:18 +1300

    Section 8.71 Datetime and keywords

--- Section8/CoundtingDocuments.py
+++ Section8/CoundtingDocuments.py
@@ -9,5 +9,25 @@ Users.find().count()
 Users.find({"favorite_number": 445}).count()
 
 #using multiple field and values to find a document
-
 Users.find({"favorite_number": 445, "username":"yetanother"}).count()
+
+#Datetime and Keyword with Pymongo
+import datetime #a base package
+
+current_date = datetime.datetime.now()
+current_date
+
+old_date = datetime.datetime(2009, 8, 11)
+uid = Users.inser({"username": "file", "date": current_date})
+
+#find users 
+#$gt refers to greater than
+Users.find({"date": {"$gt" : old_date}}).count()
+#$lt refers to less than
+Users.find({"date": {"$lt" : old_date}}).count()
+
+Users.find({"date": {"$exists" : True}}).count()
+
+#find any documents that is not equal to yetanother using $ne which means notequal
+Users.find({"date": {"$ne" : "yetanother"}}).count()
+
