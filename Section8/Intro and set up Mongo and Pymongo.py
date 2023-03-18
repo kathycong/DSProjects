530edaec31e82ab4cad6067e545cca0b59b6c44d
kathycong <kathlynycong@gmail.com>
2023-03-18 23:48:32 +1300

    Section 8.66 Introduction and setup of Pymongo

---
+++ Section8/Intro and set up Mongo and Pymongo.py
@@ -0,0 +1,12 @@
+#MongoDB is a NO SQL database
+
+#installing it via terminal 'brew install mongodb'
+
+# we need to start mongodb 'brew services start mongodb'
+
+#curl http://localhost:27017
+
+#install python package 
+#pip3 install pymongocd
+import pymongo
+
