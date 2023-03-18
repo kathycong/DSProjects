46c0b1eb583dfad3f2962d9e2dd281f90a32b814
kathycong <kathlynycong@gmail.com>
2023-03-19 00:19:47 +1300

    Section 8.70 Multiple find conditions

--- Section8/CoundtingDocuments.py
+++ Section8/CoundtingDocuments.py
@@ -7,3 +7,7 @@ Users.find().count()
 
 #counting documents given with below criteria
 Users.find({"favorite_number": 445}).count()
+
+#using multiple field and values to find a document
+
+Users.find({"favorite_number": 445, "username":"yetanother"}).count()
