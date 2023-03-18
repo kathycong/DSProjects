d4c6005e640ce2f639a27d2d1d5cbd91221318d9
kathycong <kathlynycong@gmail.com>
2023-03-19 00:09:55 +1300

    Section 8.67 Inserting Documents

---
+++ Section8/InsertingDocuments.py
@@ -0,0 +1,38 @@
+#MongoDB is a NO SQL database
+
+#installing it via terminal 'brew install mongodb'
+
+# we need to start mongodb 'brew services start mongodb'
+
+#connecting to mongodb client
+#curl http://localhost:27017 
+
+#install python package 
+#pip3 install pymongocd
+
+import pymongo
+from pymongo import MongoClient
+
+#setting up the client with default
+myClient = MongoClient()
+
+##creating a database
+db = myClient.mydb #or myClient["mydatabase"]
+
+##accessing the tables in the database
+users =db.users
+
+user1 = {"username": "nick", "password": "myverysecurepassword", "favoritenumber": 445, "hobbies": ["python", "games", "pizza"]}
+
+#inserting users to database
+user_id = users.insert_one(user1).inserted_id
+
+user1 = {"username": "anothe_user", "password": "myverysecurepassword", "favoritenumber": 445, "hobbies": ["python", "games", "pizza"]}
+
+user_id = users.insert_one(user1).inserted_id
+
+
+
+
+
+
