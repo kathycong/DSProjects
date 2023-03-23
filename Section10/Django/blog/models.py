7b0f52707153a2d2116e2d4c2425f4af6f87f286
kathycong <kathlynycong@gmail.com>
2023-03-23 23:32:48 +1300

    Section 10.93 creating blog app

---
+++ Section10/Django/blog/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+#import url resolvers
+from django.core.urlresolvers import reverse
+
+# Create your models here.
+
+#class for each blog posts
+#this class has access to all the code that's in this class and it becomes a part of this class
+class Post(models.Model):
+    #below are properties of each posts
+    title = models.CharField(max_length = 255) #inputs
+    slug = models.SlugField(max_length = 255, unique=True) #inputs
+    summary = models.CharField(max_length=300) #inputs
+    content = models.TextField() #text field
+    published = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created']
+
+        def __unicode__(self):
+            return u'%s'% self.title
+        
+    def get_absolute_url(self):
+        return reverse('blog.views.post', args = [self.slug])
+        
+
+
+
+
