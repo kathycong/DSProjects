f352e3660f1962046952fcf3794ac3e73c886bab
kathycong <kathlynycong@gmail.com>
2023-03-26 12:56:07 +1300

    11.112 Button event handling

--- Section11/calculator/main.py
+++ Section11/calculator/main.py
@@ -3,6 +3,17 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 
+class Button:
+    def __init__(self, text, results):
+        self.b = QPushButton(str(text))
+        self.text = text
+        self.results = results
+        self.b.clicked.connect(lambda: self.handleInput(self.text))
+
+    def handleInput(self, v):
+        print("clicked", v)
+
+
 class Application(QWidget):
     def __init__(self):
         super().__init__()
@@ -30,11 +41,14 @@ class Application(QWidget):
             if col > 3:
                 col = 0
                 row += 1
+
+            buttonObject = Button(button, results)
+
             if button == 0:
-                grid.addWidget(QPushButton(str(button)), row, col, 1, 2)
+                grid.addWidget(buttonObject.b, row, col, 1, 2)
                 col += 1
 
-            grid.addWidget(QPushButton(str(button)), row, col, 1, 1)
+            grid.addWidget(buttonObject.b, row, col, 1, 1)
             col += 1
 
         
