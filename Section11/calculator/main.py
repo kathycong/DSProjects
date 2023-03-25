09f50f17158691945247f536a5bf6bf1f98d23e1
kathycong <kathlynycong@gmail.com>
2023-03-26 12:24:41 +1300

    11.107 QPushButton Clicked

--- Section11/calculator/main.py
+++ Section11/calculator/main.py
@@ -8,12 +8,15 @@ class MainWindow(Qwidget):
     def __init__(self):
         super().__init__()
         self.init_ui() 
+        self.counter = 0
 
     #this will run all our code, init_ui
     def init_ui(self):
         label = QLabel("Name: ")
         name_input = QLineEdit()
-        button = QPushButton("Set Name")
+        self.button = QPushButton("Clicked: ")
+        self.button.pressed.connect(self.pressButton)
+        self.button.released.connect(self.clickedButton)
 
         h = QHBoxLayout()
         h.addStretch(1) #removed this later
@@ -23,12 +26,20 @@ class MainWindow(Qwidget):
         v = QVBoxLayout()
         v.addStretch(1) 
         v.addLayout(h)
-        v.addWidget(button)
+        v.addWidget(self.button)
 
         self.setLayout(v)
 
         self.setWindowTitle("Horizontal Layout")
         self.show()
+
+    def clickedButton(self):
+        print("This button has been released")
+
+    def pressButton(self):
+        print("This button has been pressed")
+        self.counter += 1
+        self.button.setText("Clicked: " + str(self.counter))
 
 #running the code
 if __name__ == '__main__':
