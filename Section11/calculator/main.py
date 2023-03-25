b2ea4a8605886b42f7baed157cf5ea03a38b638b
kathycong <kathlynycong@gmail.com>
2023-03-26 12:30:13 +1300

    11.108 Setting Qlabels text

--- Section11/calculator/main.py
+++ Section11/calculator/main.py
@@ -12,34 +12,38 @@ class MainWindow(Qwidget):
 
     #this will run all our code, init_ui
     def init_ui(self):
-        label = QLabel("Name: ")
-        name_input = QLineEdit()
+        self.text_label = QLabel("There has been no name entereed, so I can't do anything yet")
+        self.label = QLabel("Name: ")
+        self.name_input = QLineEdit()
         self.button = QPushButton("Clicked: ")
         self.button.pressed.connect(self.pressButton)
         self.button.released.connect(self.clickedButton)
 
+        self.button.setText("Set Name")
+        self.button.clicked.connect(self.alterName)
+
         h = QHBoxLayout()
-        h.addStretch(1) #removed this later
-        h.addWidget(label)
-        h.addWidget(name_input)
+        h.addWidget(self.label)
+        h.addWidget(self.name_input)
 
         v = QVBoxLayout()
-        v.addStretch(1) 
+        v.addWidget(self.text_label)
         v.addLayout(h)
         v.addWidget(self.button)
 
         self.setLayout(v)
 
-        self.setWindowTitle("Horizontal Layout")
+        self.setWindowTitle("Nothing has been clicked")
         self.show()
 
-    def clickedButton(self):
-        print("This button has been released")
+    def alterName(self):
+        inputted_text = self.name_input.text()
+        our_string = "You've entered " + inputted_text
+        self.text_label.setText(our_string)
+        self.setWindowTitle(inputted_text + "'s Window")
 
-    def pressButton(self):
-        print("This button has been pressed")
-        self.counter += 1
-        self.button.setText("Clicked: " + str(self.counter))
+
+
 
 #running the code
 if __name__ == '__main__':
