d4fc78b030d42d93168e789ae5c5822a6c2769c7
kathycong <kathlynycong@gmail.com>
2023-03-26 12:42:04 +1300

    11.110 GridLayout

--- Section11/calculator/main.py
+++ Section11/calculator/main.py
@@ -7,6 +7,27 @@ class Application(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Calculator")
+        self.CreateApp()
+
+    def CreateApp(self):
+        
+        #Create our grid
+        grid = QGridLayout()
+        button1 = QPushButton("One")
+        button2 = QPushButton("Two")
+        button3 = QPushButton("Three")
+        button4 = QPushButton("My Last Button")
+
+
+        grid.addWidget(button1, 0, 0, 1, 1)
+        grid.addWidget(button2, 0, 1, 1, 1)
+        grid.addWidget(button3, 0, 2, 1, 1)
+        grid.addWidget(button4, 1, 0, 1, 2)
+
+
+        
+        self.setLayout(grid)
+
         self.show()
 
 if __name__ == "__main__":
