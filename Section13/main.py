0d94f5c190486e9a5bb4a69d2e7c7b94f20a14a1
kathycong <kathlynycong@gmail.com>
2023-04-02 00:47:44 +1300

    13.136 Bar Charts part 3

--- Section13/main.py
+++ Section13/main.py
@@ -6,6 +6,7 @@ import numpy as np
 import pandas as pd
 
 col_count = 3
+bar_width = .1
 
 korea_scores = (554, 536, 538)
 canada_scores = (518, 523, 525)
@@ -14,7 +15,23 @@ france_scores = (495, 505, 499)
 
 index = np.arange(col_count)
 
-k1 = plt.bar(index, korea_scores, .5)
+k1 = plt.bar(index, korea_scores, bar_width, 
+             alpha =0.4,
+             label = 'Korea')
+c1 = plt.bar(index+0.1, canada_scores, bar_width,
+             alpha =0.4,
+             label = 'Canada')
+ch1 = plt.bar(index +0.2, china_scores, bar_width,
+              alpha =0.4,
+             label = 'China')
+f1 = plt.bar(index+0.3, france_scores, bar_width,
+              alpha =0.4,
+             label = 'France')
+plt.ylabel("Mean scire in PISA 2012")
+plt.xlabel("Subjects")
+plt.title("Test Scores by Country")
+plt.xticks(index + 0.3/2, ("Mathematics", "Reading", "Science"))
+plt.legend()
 plt.grid(True)
 plt.show()
 
