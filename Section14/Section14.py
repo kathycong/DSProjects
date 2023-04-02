63ad2c0ef538731eebbae40bb1590981be4370ba
kathycong <kathlynycong@gmail.com>
2023-04-02 21:06:25 +1200

    Feedback sounds with PyAudio

--- Section14/Section14.py
+++ Section14/Section14.py
@@ -1,1 +1,6 @@
 #prerequisites on machine learning
+
+#link to installing pyaudtio with M1 
+#https://stackoverflow.com/questions/73268630/error-could-not-build-wheels-for-pyaudio-which-is-required-to-install-pyprojec 
+pip3 install pyaudio
+pip3 install speechrecognition
---
+++ Section14/audio.py
@@ -0,0 +1,36 @@
+
+#tried below but didn't work
+#pip3 install pyglet
+#import pyglet
+#file = pyglet.resource.media('audio/case-closed-531.mp3')
+#file.play()
+#pyglet.app.run()
+
+import pyaudio
+import wave
+
+
+def play_audio(filename):
+    chunk = 1024
+    wf = wave.open(filename, 'rb') #opening the file as read binary
+    pa = pyaudio.PyAudio() #instatiate pyaudio
+
+    #used to stream the audio
+    stream = pa.open(
+        format = pa.get_format_from_width(wf.getsampwidth()),
+        channels = wf.getnchannels(),
+        rate = wf.getframerate(),
+        output = True
+    )
+
+    data_stream = wf.readframes(chunk)
+
+    while data_stream:
+        stream.write(data_stream)
+        data_stream = wf.readframes(chunk)
+
+    stream.close()
+    pa.terminate()
+
+play_audio("./audio/case-closed-531.wav")
+
